029224785ff26b954cec4bd55c704b8be89f58e1
Sachin Kumar <sk68095882122@gmail.com>
2026-09-02 23:21:34 +0530

    Add files via upload

---
+++ NextGreaterElement.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class NextGreaterElement {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Stack<Integer> st = new Stack<>();
+        int[] result = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() <= nums[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                result[i] = -1;
+            } else {
+                result[i] = st.peek();
+            }
+
+            st.push(nums[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(result[i] + " ");
+        }
+
+        sc.close();
+    }
+}
